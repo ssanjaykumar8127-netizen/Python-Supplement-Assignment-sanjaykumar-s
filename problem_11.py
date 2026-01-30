560dbf31f5c57e583d3c7f768ca7ae4abe90bf5f
ssanjaykumar8127-netizen <ssanjaykumar8127@gmail.com>
2026-01-30 15:49:04 +0530

    changed python_11.py

--- problem_11.py
+++ problem_11.py
@@ -5,7 +5,7 @@ text = "programming"
 char_count = {}
 for char in text:
     if char in char_count:
-        char_count[char] += 1
+        char_count[char]+= 1
     else:
         char_count[char] = 1
 print(char_count)
