b86946e494aef8ce5ba34488609a8716ba0cfaa2
ssanjaykumar8127-netizen <ssanjaykumar8127@gmail.com>
2026-01-30 16:09:33 +0530

    changed python_23.py

--- problem_23.py
+++ problem_23.py
@@ -6,7 +6,7 @@ positive = 0
 negative = 0
 for num in numbers:
     if num > 0:
-        positive += 1
+        positive +=1
     else:
         negative += 1
 print(f"Positive: {positive}, Negative: {negative}")
