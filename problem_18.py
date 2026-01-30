25cbb58283ea7f62fd1b09c9312bb886adc51e30
ssanjaykumar8127-netizen <ssanjaykumar8127@gmail.com>
2026-01-30 16:04:02 +0530

    changed python_18.py

--- problem_18.py
+++ problem_18.py
@@ -2,5 +2,5 @@
 # Find and fix the error
 
 sentence = "Python is a great programming language"
-word_count = len(sentence.split())
+word_count =len(sentence.split())
 print(f"Number of words: {word_count}")
