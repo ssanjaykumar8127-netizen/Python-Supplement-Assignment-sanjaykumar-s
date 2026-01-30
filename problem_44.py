cac22d8bd87e874c535bcb2384aa8533c46d39fe
ssanjaykumar8127-netizen <232771209+ssanjaykumar8127-netizen@users.noreply.github.com>
2026-01-30 19:05:05 +0530

    changed python_44.py
    Fix the Fibonacci function to handle edge cases.

--- problem_44.py
+++ problem_44.py
@@ -2,6 +2,11 @@
 # Find and fix the error
 
 def fibonacci(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    
     fib = [0, 1]
     for i in range(2, n):
         fib.append(fib[i-1] + fib[i-2])
