b77f94181a06c0c539d2cbe93fe86d739ccd66da
ssanjaykumar8127-netizen <232771209+ssanjaykumar8127-netizen@users.noreply.github.com>
2026-01-30 19:14:47 +0530

    changed python_51.py

--- problem_51.py
+++ problem_51.py
@@ -2,11 +2,8 @@
 # Find and fix the error
 
 def reverse_words(sentence):
-    words = sentence.split()
-    reversed_words = []
-    for word in words:
-        reversed_words.append(word[::-1])
-    return " ".join(reversed_words)
+    # Split the sentence into words, reverse each word, and join back
+    return " ".join(word[::-1] for word in sentence.split())
 
 text = "Hello World"
 print(f"Reversed words: {reverse_words(text)}")
